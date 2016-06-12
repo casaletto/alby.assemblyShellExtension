5e80eda7ef6730aa7c683ff9eb4c10c56fcd4a72
casaletto <casaletto@users.noreply.github.com>
2016-06-12 19:30:03 +1000

    bumped up version number 1.0.0.4 i realllllllllllly need to do the build.proj /choco packaging

--- assemblyAttributes/Properties/AssemblyInfo.cs
+++ assemblyAttributes/Properties/AssemblyInfo.cs
@@ -6,17 +6,17 @@ using assemblyAttributes ;
 [assembly: AssemblyTitle("alby.assemblyAttributes")]
 [assembly: AssemblyDescription("alby.assemblyAttributes")]
 [assembly: AssemblyConfiguration("alby.assemblyAttributes")]
-[assembly: AssemblyCompany("casaletto PL")]
+[assembly: AssemblyCompany("Casaletto PL")]
 [assembly: AssemblyProduct("alby.assemblyAttributes")]
-[assembly: AssemblyCopyright("copyright ©  2016 Casaletto PL")]
+[assembly: AssemblyCopyright("© 2016 Casaletto PL. All rights reserved. MIT licence.")]
 [assembly: AssemblyTrademark("Casaletto PL")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4fef644f-53b6-4bfe-93bf-9328ce6e2147")]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
-[assembly: AssemblyInformationalVersion( "1.0.0.3") ]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyInformationalVersion( "1.0.0.4") ]
 
 [assembly: MyCustomAssemblyAttribute("Custom attribute text")]
 
--- assemblyAttributes32/Properties/AssemblyInfo.cs
+++ assemblyAttributes32/Properties/AssemblyInfo.cs
@@ -2,35 +2,18 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("assemblyAttributes32")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("assemblyAttributes32")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("alby.assemblyAttributes32")]
+[assembly: AssemblyDescription("alby.assemblyAttributes32")]
+[assembly: AssemblyConfiguration("alby.assemblyAttributes32")]
+[assembly: AssemblyCompany("casaletto PL")]
+[assembly: AssemblyProduct("alby.assemblyAttributes32")]
+[assembly: AssemblyCopyright("© 2016 Casaletto PL. All rights reserved. MIT licence.")]
+[assembly: AssemblyTrademark("Casaletto PL")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: Guid("4fef644f-53b6-4bfe-93bf-9328ce6e2147")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5f776ac0-34a9-4627-bde8-e082e13ae833")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyInformationalVersion("1.0.0.4")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
